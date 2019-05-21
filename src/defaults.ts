87444fe5dbff118f3be48b0c9f83123af6764702
Raiondesu <a.raiondesu@gmail.com>
2019-05-22 00:24:32 +0300

    Add defaults - separate all defaults logic

---
+++ src/defaults.ts
@@ -0,0 +1,20 @@
+import { OptionObject } from 'loader-utils';
+import { schema, Options } from './options';
+
+export const defaults: Options = {
+  viewFileName: 'view',
+  viewFileExt: '.pug',
+  styleFileName: 'styles',
+  styleFileExt: '.styl'
+};
+
+export const optionsWithDefaults = (options: OptionObject & Options) => {
+  for (const key in schema.properties) {
+    options[key] = typeof options[key] === 'string' ? options[key] : defaults[key];
+  }
+
+  options.styleFileName = options.styleFileName + options.styleFileExt;
+  options.viewFileName = options.viewFileName + options.viewFileExt;
+
+  return options;
+}
