14c477414486c170ef18380635d824c4069894f6
Raiondesu <a.raiondesu@gmail.com>
2019-05-22 00:25:09 +0300

    Refactor loader - in cohesion with newly added modules

--- src/loader.ts
+++ src/loader.ts
@@ -1,113 +1,48 @@
-import { getOptions } from 'loader-utils';
-import validateOptions from 'schema-utils';
-import ts from 'typescript';
 import { existsSync } from 'fs';
 import { join } from 'path';
+import { validateOptions } from './options';
+import { loader } from 'webpack';
+import { optionsWithDefaults } from './defaults';
 
-const schema = {
-  type: 'object',
-  properties: {
-    viewFileName: {
-      type: 'string'
-    },
-    viewFileExt: {
-      type: 'string'
-    },
-    styleFileName: {
-      type: 'string'
-    },
-    styleFileExt: {
-      type: 'string'
-    }
-  }
-};
+import {
+  importRenderDeclaration,
+  importStyleDeclaration,
+  createFile,
+  printer,
+  findDefaultExport,
+  generateImportDeclarations,
+  updateSourceWith
+} from './ts-utils';
 
-const defaultViewFileName = 'view';
-const defaultViewFileExt = '.pug';
-const defaultStyleFileName = 'styles';
-const defaultStyleFileExt = '.styl';
+export default function (this: loader.LoaderContext, source: string) {
+  const options = validateOptions.call(this, 'Mantha component loader');
+  const fileExists = (filename: string) => existsSync(join(this.context, filename));
 
-export default function (source) {
-  const options = getOptions(this);
-
-  validateOptions(schema, options, 'Mantha component loader');
-
-  const literal = (...args: Parameters<typeof ts.createLiteral>) => {
-    const result = ts.createLiteral(...args);
-    (result as any).singleQuote = true;
-    return result;
-  }
-
-  const { viewFileName, styleFileName, viewFileExt, styleFileExt } = options;
-  const importStatements: ts.ImportDeclaration[] = [];
-  const finalViewFileExt = viewFileExt || defaultViewFileExt;
-  const finalViewFileName = (viewFileName || defaultViewFileName) + finalViewFileExt;
-  const finalStyleFileExt = styleFileExt || defaultStyleFileExt;
-  const finalStyleFileName = (styleFileName || defaultStyleFileName) + finalStyleFileExt;
-
-  let renderPresent = false;
-
-  // Add view import statement if there is such file
-  if (existsSync(join(this._module.context, finalViewFileName))) {
-    renderPresent = true;
-    importStatements.push(ts.createImportDeclaration(
-      undefined,
-      undefined,
-      ts.createImportClause(
-        ts.createIdentifier('render'),
-        undefined
-      ),
-      literal(`./${finalViewFileName}`)
-    ));
-  }
-
-  // Add styles import statement if there is such file
-  if (existsSync(join(this._module.context, finalStyleFileName))) {
-    importStatements.push(ts.createImportDeclaration(
-      undefined,
-      undefined,
-      undefined,
-      literal(`./${finalStyleFileName}`)
-    ));
-  }
-
-  const sourceFile = ts.createSourceFile(
-    'index.ts',
-    source,
-    ts.ScriptTarget.ESNext,
-    true,
-    ts.ScriptKind.TS
+  const { viewFileName, styleFileName } = optionsWithDefaults(options);
+  const importStatements = generateImportDeclarations(
+    [styleFileName, viewFileName],
+    [importStyleDeclaration, importRenderDeclaration],
+    fileExists
   );
 
-  const printer = ts.createPrinter({
-    omitTrailingSemicolon: false,
-    removeComments: false
-  });
-
-  let update = sourceFile;
-
-  if (renderPresent) {
-    let updatedNode: ts.Node | null = null;
-    ts.forEachChild(sourceFile, node => {
-      if (node.kind === ts.SyntaxKind.ExportAssignment && node.getChildAt(1).kind === ts.SyntaxKind.DefaultKeyword) {
-        updatedNode = node;
-      }
-    });
-    
-    if (updatedNode) {
-      const _updatedNode: ts.Node = updatedNode;
-      const exportedStatement = _updatedNode.getChildAt(2);
-      const renderCall = ts.createCall(ts.createIdentifier('render'), undefined, [exportedStatement as ts.Expression]);
-      const indexOfExport = sourceFile.statements.indexOf(_updatedNode as ts.Statement);
-
-      update = ts.updateSourceFileNode(sourceFile, [
-        ...importStatements,
-        ...sourceFile.statements.slice(0, indexOfExport),
-        ts.createExportDefault(renderCall),
-        ...sourceFile.statements.slice(indexOfExport+1)
-      ]);
-    }
+  // Return original file if no imports need to be added
+  if (importStatements.length === 0) {
+    return source;
   }
 
-  return printer.printFile(update);
+  const sourceFile = createFile(source);
+  const defaultExport = findDefaultExport(sourceFile);
+
+  // Return original file if no default export is found
+  if (!defaultExport) {
+    return sourceFile;
+  }
+
+  return printer.printFile(
+    updateSourceWith({
+      sourceFile,
+      importStatements,
+      inject: defaultExport
+    })
+  );
 }
